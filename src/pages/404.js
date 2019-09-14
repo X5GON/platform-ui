5648974c636ec1924fe30d73eb7960a3e694ebe4
gregpr07 <gregor.ninjaboy@gmail.com>
2019-09-14 20:53:48 +0200

    added 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,20 @@
 import React from "react";
+import { Navbar, Footer } from "../Components";
 
 class Products extends React.Component {
   render() {
-    return <div className="404">This site does not exist</div>;
+    return (
+      <div className="bg-black text-white">
+        <Navbar />
+        <div
+          className="bg-sky text-center"
+          style={{ minHeight: "50vh", paddingTop: "15vh" }}
+        >
+          <h1>ERROR 404:</h1> This site does not exist
+        </div>
+        <Footer />
+      </div>
+    );
   }
 }
 
